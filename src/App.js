384c7dff18f5652e414213f5da0ac4245ad4f9cb
sashakalinin <sashakalininweb@gmail.com>
2020-04-22 15:44:46 +0300

    asda1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
 
 
 
@@ -7,30 +10,9 @@ import './App.css';
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png"></img>
-      </header>
-      <nav className='nav'>
-        <div>Profile</div>
-        <div>Messeges</div>
-        <div>News</div>
-        <div>Music</div>
-        <div>Settings</div>
-      </nav>
-      <div className='content'>
-        <div className='content-img-bg'>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTNl83-Gff4Wev5w1sXepNoGSfHcwGkMFnyTStiT8o2pHC5LXjj&usqp=CAU' />
-        </div>
-        <div className='content-img-avatar'>
-          <img src='https://st3.depositphotos.com/5366154/13886/i/450/depositphotos_138866008-stock-photo-fire-letter-v-of-burning.jpg' />
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Nav />
+      <Profile />
     </div>
 
   );
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className='content-img-bg'>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTNl83-Gff4Wev5w1sXepNoGSfHcwGkMFnyTStiT8o2pHC5LXjj&usqp=CAU' />
+            </div>
+            <div className='content-img-avatar'>
+                <img src='https://st3.depositphotos.com/5366154/13886/i/450/depositphotos_138866008-stock-photo-fire-letter-v-of-burning.jpg' />
+            </div>
+            <div>
+                My posts
+          <div>
+                New post
+          </div>
+                <div>
+                    post 1
+            </div>
+                <div>
+                    post 2
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
