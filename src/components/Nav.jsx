dad62464bccd54d7deeb461f825f038fd444556f
sashakalinin <sashakalininweb@gmail.com>
2020-04-23 15:47:22 +0300

    123123

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 const Nav = () => {
     return (
         <nav className='nav'>
-            <div className='nav-menu'><a href='#'>Hot Profile</a></div>
-            <div className='nav-menu'><a href='#'>Hot Messeges</a></div>
-            <div className='nav-menu'><a href='#'>Hot News</a></div>
-            <div className='nav-menu'><a href='#'>Hot Music</a></div>
-            <div className='nav-menu'><a href='#'>Hot Settings</a></div>
+            <div className='nav-inner'>
+                <div className='nav-menu'>
+                    <a href='#'>Hot Profile</a>
+                    <a href='#'>Hot Messeges</a>
+                    <a href='#'>Hot News</a>
+                    <a href='#'>Hot Music</a>
+                    <a href='#'>Hot Settings</a>
+                </div>
+            </div>
         </nav>
     );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,29 +4,38 @@ const Profile = () => {
     return (
         <div className='content'>
             <div className="about_mi-all">
-                
-                    <img className='avatar' src='https://gtavrl.ru/public/poscreenra.jpg' />
-                
+
+                <img className='avatar' src='https://gtavrl.ru/public/poscreenra.jpg' />
+
                 <div className="about_mi">
-                    <p className="about_mi-inner">Tom Hardy</p>
-                    <p className="about_mi-inner">Date of Birth: 15 september</p>
-                    <p className="about_mi-inner">City:Minsk</p>
-                    <p className="about_mi-inner">Education:BSUIR</p>
-                    <p className="about_mi-inner">Web Site: https://tomhardy.com</p>
+                    <div className='about_mi-inner'>
+                        <p className="about_mi-info">Tom Hardy</p>
+                        <p className="about_mi-info">Date of Birth: 15 september</p>
+                        <p className="about_mi-info">City:Minsk</p>
+                        <p className="about_mi-info">Education:BSUIR</p>
+                        <p className="about_mi-info">Web Site: https://tomhardy.com</p>
+                    </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    New post
-          </div>
-                <div>
-                    post 1
+
+            <div className='my-post'>
+                <div className='my-post-inner'>
+                    <div className='my-post-title'>
+                        <p className='post-title'>My posts</p>
+                    </div>
+                    <form className='my-post-write'>
+                        <textarea placeholder='your news...' form="" name="your-mews" required ></textarea>
+                        <button href='#'>Send</button>
+                    </form>
+                </div>
+                <div className='my-post-share'>
+                    <div className='post-circle'></div><p className='post'>Hey, why how are you</p>
+                </div>
+                <div className='my-post-share'>
+                <div className='post-circle'></div><p className='post'>Im lost yourself</p>
+                </div>
             </div>
-                <div>
-                    post 2
-            </div>
-            </div>
-        </div>
+        </div >
     );
 }
 
